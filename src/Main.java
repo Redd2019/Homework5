6dece531bb431e1151fcdd1e15ea80b334d1bbb6
Redd2019 <anton-mikerin@yandex.ru>
2023-01-22 16:50:44 +0300

    Выполнено задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
 
@@ -17,5 +18,25 @@ public class Main {
                     default:
                         System.out.println("Добро пожаловать в браузерную версию!");
                 }
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        int clientOs = 0;
+        int clientDeviceYear = 2014;
+        if (clientDeviceYear <= 2015) {
+            if (clientOs == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientDeviceYear > 2015) {
+            if (clientOs == 1){
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+            if (clientOs==0){
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+            }
     }
 }

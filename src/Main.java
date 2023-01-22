a25ef3ca534e09e3c110d4204c2eaba5232b16ef
Redd2019 <anton-mikerin@yandex.ru>
2023-01-22 16:35:20 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        int clientOs = 1;
+                switch (clientOs) {
+                    case 0:
+                        System.out.println("Установите версию приложения для iOS по ссылке");
+                        break;
+                    case 1:
+                        System.out.println("Установите версию приложения для Android по ссылке");
+                        break;
+                    default:
+                        System.out.println("Добро пожаловать в браузерную версию!");
+                }
     }
 }

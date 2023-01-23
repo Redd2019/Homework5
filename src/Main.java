db4ac74828d065911f733a080549532c45d16ac3
Redd2019 <anton-mikerin@yandex.ru>
2023-01-23 10:29:12 +0300

    Выполнено задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
 
     }
 
@@ -38,5 +40,18 @@ public class Main {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
             }
+    }
+
+    public static void task3() {
+        int year = 2021;
+        if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");;
+        }
+    }
+
+    public static void task4() {
+
     }
 }

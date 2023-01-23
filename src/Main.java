8c41dbf9013e2ce1a3d348c4ef4697f59ae9b592
Redd2019 <anton-mikerin@yandex.ru>
2023-01-23 11:43:25 +0300

    Выполнено задание 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
 
     }
 
@@ -70,6 +71,52 @@ public class Main {
             if (deliveryDistance >100) {
                 System.out.println("Доставка не осуществляется.");
             }
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задание 5");
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Неверное значение");
         }
     }
 }
